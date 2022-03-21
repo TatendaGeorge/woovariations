7ee5da7248e1f12383eaa749462a24c80d9b1ca3
TatendaGeorge <georgetatenda27@gmail.com>
2022-03-21 20:32:59 +0200

    Initial commit

---
+++ woovariations.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Woo Variations
+ *
+ * @package           PluginPackage
+ * @author            OboxThemes
+ * @copyright         2022 OboxThemes
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Woo Variations
+ * Plugin URI:        https://oboxthemes.com/
+ * Description:       Woo Variations lets you create product variations separately and quickly assign them to products.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            OboxThemes
+ * Author URI:        https://oboxthemes.com
+ * Text Domain:       woo-variations
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Update URI:        https://oboxthemes.com/woo-variations/
+ */
